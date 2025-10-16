eb91fd780ba8b46fdb36552ad00ed5f0368f7633
fbwoduf112 <fbwoduf2@gmail.com>
2025-10-16 18:35:53 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 14836 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int dp [] = new int [n];
+		int arr [] = new int [n];
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		dp[0] = arr[0];
+		if(n>1) dp[1] = arr[0] + arr[1];
+		if(n>2) dp[2] = Math.max(dp[1], Math.max(arr[0] + arr[2], arr[1] + arr[2]));
+		
+		for(int i=3; i<n; i++) {
+			dp[i] = Math.max(dp[i-1], Math.max(dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i]));
+		}
+		
+		System.out.println(dp[n-1]);
+	}
+}
