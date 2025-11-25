82cd8338b7cf17991643fa4a66e63948200984d6
fbwoduf112 <fbwoduf2@gmail.com>
2025-11-25 20:00:00 +0900

    [Silver I] Title: 경로 찾기, Time: 304 ms, Memory: 18948 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,47 +1,45 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 
 public class Main {
-	static int arr[][];
-	static boolean check[];
-	static int ans[][];
-	static int N;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int [][] graph = new int[N][N];
+		int [][] ans = new int[N][N];
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j=0; j<N; j++) {
+				graph[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=0; i<N; i++) { //중간
+			for(int j=0; j<N; j++) { //시작
+				
+				if(graph[j][i] == 0) continue;
+				ans[j][i] = 1;
+				for(int s=0; s<N; s++) { //끝
+					if(graph[i][s] == 0) continue;
+					graph[j][s] = 1;
+					ans[j][s] = 1;
+				}
+			}
+		}
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				System.out.print(ans[i][j] + " ");
+			}
+			System.out.println();
+		}
 
-        N = Integer.parseInt(br.readLine());
-        arr = new int [N][N];
-        check = new boolean[N];
-        ans = new int[N][N];
-        StringTokenizer st;
-        for(int i=0; i<N; i++) {
-        	st = new StringTokenizer(br.readLine(), " ");
-        	for(int j=0; j<N; j++) {
-        		arr[i][j] = Integer.parseInt(st.nextToken());
-        	}
-        }
-        
-        for(int i=0; i<N; i++) {
-        	for(int j=0; j<N; j++) {
-        		if(arr[i][j] == 1) dfs(i, j);
-        	}
-        	Arrays.fill(check, false);
-        }
-        
-        for(int i=0; i<N; i++) {
-        	for(int j=0; j<N; j++) {
-        		System.out.print(ans[i][j] + " ");
-        	}
-        	System.out.println();
-        }
-    }
-    static void dfs(int num, int path) {
-    	check[path]=true;
-    	ans[num][path] = 1;
-    	for(int i=0; i<N; i++) {
-    		if(!check[i] && arr[path][i] == 1) {
-    			dfs(num, i);
-    		}
-    	}
-    }
+	}
 }
